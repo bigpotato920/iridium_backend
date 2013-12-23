d3e1e9871e9fb3fa9f36a9998f1d71df27873731
bigpotato920 <732792753@qq.com>
2013-12-23 09:35:00 +0800

    给分片消息添加端口号成员

--- iridium.c
+++ iridium.c
@@ -2,11 +2,79 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-#include "iridium.h"
 #include "network.h"
 
+#define SLICE_LEN 30
+#define SBD_MO_HEADER_LEN 51
+#define MAX_PACKET_SIZE 1960
+#define MAX_IRIDIUM_NUM 3
 
+typedef struct
+{
+	int sn; // sequential number of the iridium terminal
+	unsigned long ip; // destination ip address
+    int port; // port num
+	int index; // index of the specific slice
+	int count; // totoal count of the slices
+
+} slice_header;
+
+#define SLICE_HEADER_LEN sizeof(slice_header)
+#define SLICE_PAYLOAD_LEN (SLICE_LEN-SLICE_HEADER_LEN)
+
+typedef struct 
+{
+	slice_header header;
+	char payload[];
+} slice;
+
+typedef struct 
+{
+	slice_header header;
+	int current_count;
+	int payload_len;
+	char payload[MAX_PACKET_SIZE];
+} mo_msg;
+
+typedef struct
+{
+	uint8_t pro_num;
+	uint16_t msg_len;
+	uint8_t header_iei;
+	uint16_t header_len;
+	char client_msg_id[4];
+	char imei[15];
+	uint16_t flag;
+	uint8_t payload_iei;
+	uint16_t payload_len;
+	char payload[];
+
+} iridium_mt_msg;
+
+
+typedef struct 
+{
+	char imei[15];
+	char command[];
+
+} server_command;
+
+#define MO_HEADER_LEN sizeof(slice)
+#define SBD_MT_HEADER_LEN sizeof(iridium_mt_msg)
+
+
+
+int init();
+
+int get_payload(char* msg_recv);
+int get_slice(int client_fd, slice* slice);
+int receive_iridium_msgs(int server_fd);
+int resemble_iridium_msgs(slice* slice, int payload_len);
+int forward_iridium_msg (int sn);
+int forward_server_command(int unix_server_fd, int tcp_client_fd);
+int sendto_iridium(int tcp_client_fd, const char* imei, const char* command);
 mo_msg mo_msgs[MAX_IRIDIUM_NUM];
 
 int init()
@@ -74,6 +142,7 @@ int resemble_iridium_msgs(slice *slice, int payload_len)
 	int sn = slice->header.sn;
 	int index = slice->header.index;
 	mo_msgs[sn].header = slice->header;
+	int i;
 	mo_msgs[sn].payload_len += payload_len;
 	mo_msgs[sn].current_count += 1;
 	memcpy(mo_msgs[sn].payload + index*(SLICE_PAYLOAD_LEN), slice->payload,
@@ -84,6 +153,11 @@ int resemble_iridium_msgs(slice *slice, int payload_len)
 		mo_msgs[sn].current_count = 0;
 		mo_msgs[sn].payload[mo_msgs[sn].payload_len] = '\0';
 		printf("total payload len = %d\n", mo_msgs[sn].payload_len);
+		
+
+		for (i = 0; i < mo_msgs[sn].payload_len; i++)
+			printf("%c", mo_msgs[sn].payload[i]);
+		printf("\n");
 		mo_msgs[sn].payload_len = 0;
 
 		return 1;
@@ -101,9 +175,11 @@ int resemble_iridium_msgs(slice *slice, int payload_len)
  */
 int forward_iridium_msg (int sn) 
 {
-	long ip = mo_msgs[sn].header.ip;
-	printf("ip = %lu, len = %d, forward_msg = %s\n", ip, strlen(mo_msgs[sn].payload),mo_msgs[sn].payload);
-	return send_to_udp_server(ip, mo_msgs[sn].payload);
+	unsigned long ip = mo_msgs[sn].header.ip;
+	int port = mo_msgs[sn].header.port;
+	printf("ip = %lu, port = %d, len = %d, forward_msg = %s\n", 
+		ip, mo_msgs[sn].header.port, mo_msgs[sn].payload_len, mo_msgs[sn].payload);
+	return send_to_udp_server(ip, port, mo_msgs[sn].payload);
 }
 
 
